f06f2cce591e24d4533e64db7ce5f2e0e330542b
adricd <adrihanacd@gmail.com>
2024-09-18 22:05:11 -0500

    cambios

---
+++ src/main/java/com/prestamo/repository/CatalogoRepository.java
@@ -0,0 +1,11 @@
+package com.prestamo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prestamo.entity.Catalogo;
+
+public interface CatalogoRepository extends JpaRepository<Catalogo, Integer> {
+
+
+}
+
